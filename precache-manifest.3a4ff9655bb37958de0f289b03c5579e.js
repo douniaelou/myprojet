4223bfd0e162e25ed975a4909a81cb939e149cc2
Dounia El ouaddane <douniaelouaddane@MacBook-Pro-de-Dounia.local>
2020-01-08 15:29:33 +0100

    Updates

---
+++ precache-manifest.3a4ff9655bb37958de0f289b03c5579e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5eba1a4811cbc4cd3a29342e530511b5",
+    "url": "/myprojet/index.html"
+  },
+  {
+    "revision": "06ea292c7b649986fbb3",
+    "url": "/myprojet/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1a45d29e1f2509ecbf84",
+    "url": "/myprojet/static/js/2.a8816a2f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/myprojet/static/js/2.a8816a2f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "06ea292c7b649986fbb3",
+    "url": "/myprojet/static/js/main.eb9c1d0a.chunk.js"
+  },
+  {
+    "revision": "c3fc0f162ad747636b7e",
+    "url": "/myprojet/static/js/runtime-main.7214e570.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myprojet/static/media/logo.5d5d9eef.svg"
+  }
+]);
